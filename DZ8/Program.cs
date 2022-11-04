5f9829d7dd277f15d1a64b6cc9d70c021cfc04f2
Anna <radiant2958@gmail.com>
2022-11-04 18:07:55 +0300

    Задача19

---
+++ DZ8/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19
+
+int[] MetodArray(int number)
+{
+    int[] array = new int[5];
+    
+        for (int i = 0; i<array.Length;i++)
+        {
+           int k= number%10;
+           array[i]=k;
+           number=number/10; 
+        }
+    return array;
+
+}
+       
+
+
+void VerificationNumber(int number)
+{
+    int[] array1 = MetodArray(number);
+    bool a;
+    if (array1[0]==array1[4] | array1[1]==array1[3])
+    {
+       a=true;
+    }
+    else
+    {
+       a=false;
+
+    }
+    Console.WriteLine(a);
+}
+  
+VerificationNumber(12321);
