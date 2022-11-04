5c8f165633d455e585c823c71f74a6374a07b981
Anna <radiant2958@gmail.com>
2022-11-04 18:23:04 +0300

    Задача21

---
+++ DZ10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 21
+
+double DistancePoints(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double rezult = Math.Sqrt((x2-x1)+(y2-y1)+(z2-z1));
+    return rezult;
+}
+
+Console.WriteLine("Введите координаты первой точки А");
+bool inputX1 = int.TryParse(Console.ReadLine(),out int xA);
+bool inputY1 = int.TryParse(Console.ReadLine(),out int yA);
+bool inputZ1 = int.TryParse(Console.ReadLine(),out int zA);
+
+Console.WriteLine("Введите координаты второй точки В");
+bool inputX2 = int.TryParse(Console.ReadLine(),out int xB);
+bool inputY2 = int.TryParse(Console.ReadLine(),out int yB);
+bool inputZ2 = int.TryParse(Console.ReadLine(),out int zB);
+
+double n=DistancePoints(xA,yA,zA,xB,yB,zB);
+Console.WriteLine(n);
