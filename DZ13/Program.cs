b526cfc02d771df51b01a6bad5a0d9adfda07950
Anna <radiant2958@gmail.com>
2022-11-04 18:30:00 +0300

    Задача29

---
+++ DZ13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Задание должно быть выполнено в методах.
+
+int[] GenerateArr ()
+{
+    int[] array = new int[8];
+    Random random = new Random();
+    for( int i= 0; i<array.Length;i++)
+    {
+        array[i]= random.Next(0,99);
+
+
+    }
+    return array;
+}
+
+int[] array = GenerateArr();
+
+void OutPut(int[] array1)
+{
+    for(int i = 0; i<array1.Length;i++)
+    {
+        Console.Write($"{array1[i]} " );
+
+    }
+    
+}
+ 
+ OutPut(GenerateArr());
