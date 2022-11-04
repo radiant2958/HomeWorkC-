817f743b1fd8a9a698dc00980d7efef6c5e293b7
Anna <radiant2958@gmail.com>
2022-11-04 18:16:03 +0300

    Задача23

---
+++ DZ9/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+int[] FunctionCub(int n)
+{
+    int[] array = new int[n];
+    int j=1;
+    for(int i=0; i<array.Length; i++)
+    {
+        
+        array[i]=j*j*j;
+        j++;
+    }
+    return array;
+}
+
+void OutPut(int[] array1)
+{
+    for(int i= 0; i<array1.Length;i++)
+    {
+        Console.Write($"{array1[i]} ");
+
+    }
+}
+OutPut(FunctionCub(5));
