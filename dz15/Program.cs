361aa4dfd384cc061535c5606ae9d6ec29f2cb88
Anna <radiant2958@gmail.com>
2022-11-12 22:02:59 +0300

    Задача36

---
+++ dz15/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int [] array = new int[7];
+FillArray(array);
+PrintArray(array);
+int result=SumEvenElemArray(array);
+Console.WriteLine(result);
+
+int SumEvenElemArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+        sum=array[i]+sum;
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine();
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+}
