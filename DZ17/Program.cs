ddc20ba0e08588df7f37fcb4861180e701938068
Anna <radiant2958@gmail.com>
2022-11-19 19:56:19 +0300

    Задача41

---
+++ DZ17/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите колличество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PositiveIntroducedNumber(NewArray(m)));
+
+int PositiveIntroducedNumber(int[] array)
+{
+    int sum=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+
+int[] NewArray(int m)
+{
+    int [] array=new int [m];
+    for (int i=0; i<m; i++)
+    {
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
