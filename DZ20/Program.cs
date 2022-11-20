d18c3174263f2a505da36650fc7a0357a2527089
Anna <radiant2958@gmail.com>
2022-11-20 13:29:53 +0300

    Задача50

---
+++ DZ20/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//1 , 7 -> такого числа в массиве нет
+//1 , 1 -> 1
+Console.WriteLine("Введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+int[,] array = CreateRandom2DArray(3, 4);
+Print2DArray(array);
+Console.WriteLine(ValueElementAray(array, m, n));
+
+int ValueElementAray(int[,] array, int m, int n)
+{
+    int value = 0;
+    if (m < 0 | m > array.GetLength(0) | n < 0 | n > array.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет в массиве");
+    }
+    
+    value = array[m, n];
+    
+    return value;
+
+}
+
+
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
