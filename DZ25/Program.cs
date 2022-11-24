49eb673617a9692871f9be4170a8d281e828dfc6
Anna <radiant2958@gmail.com>
2022-11-24 19:11:15 +0300

    Задача60

---
+++ DZ25/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите метод, который будет построчно выводить массив, добавляя индексы каждого элемента.
+Console.WriteLine("Введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+Console.WriteLine("Введите k - ");
+bool isParsedK = int.TryParse(Console.ReadLine(), out int k);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+int[,,] array = CreateRandom3DArray(m, n, k);
+Print3DArray(array);
+
+int[,,] CreateRandom3DArray(int m, int n, int k)
+{
+    int[,,] array = new int[m, n, k];
+
+    Random random = new Random();
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            for (int p = 0; p < array.GetLength(2); p++)
+            {
+                array[i, j, p] = random.Next(10, 99);
+            }
+
+        }
+    }
+
+    return array;
+}
+
+void Print3DArray(int[,,] array)
+{
+    for (var p = 0; p < array.GetLength(2); p++)
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j, p]} {(i,j,p)} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
