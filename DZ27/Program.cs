26e8a7f7bb6baecc80d81fbdc6d2bad470ef476a
Anna <radiant2958@gmail.com>
2022-11-27 18:42:42 +0300

    Задача66

---
+++ DZ27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите метод, который найдёт сумму натуральных элементов в промежутке от M до N. Через рекурсию!
+
+int SumNumber(int a, int b)
+{
+    int sum=0;
+    if(b==0) return a;
+    else if(a<=b)
+    {
+        sum=a+SumNumber(a+1,b);
+    }
+    return sum;
+}
+
+Console.WriteLine(SumNumber(2,5));
