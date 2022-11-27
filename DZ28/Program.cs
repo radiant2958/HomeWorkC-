33ec55eae3cb06bd9cd8086d0de512caf83bf738
Anna <radiant2958@gmail.com>
2022-11-27 19:27:39 +0300

    Задача68

---
+++ DZ28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите метод вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+Console.WriteLine("Введите m");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FunctionAckerman(m,n));
+
+int FunctionAckerman(int m,int n)
+{
+    int fun=0;
+    if(m==0)
+    {
+        fun= n+1;
+    }
+    else if(m>0 & n==0)
+    {
+        fun = FunctionAckerman(m-1,n);
+    }
+    else if(m>0&n>0)
+    {
+        fun=FunctionAckerman(m-1,FunctionAckerman(m,n-1));
+    }
+    return fun;
+
+}
